c7f8625f9f62cd2c2597fd806e1c44a8b593ca6a
Gustavo Mello <oliver7mello@gmail.com>
2024-11-10 23:57:56 -0300

    modularizando o menu

--- src/main/java/br/com/mello/springscreenmatch/SpringscreenmatchApplication.java
+++ src/main/java/br/com/mello/springscreenmatch/SpringscreenmatchApplication.java
@@ -1,10 +1,7 @@
 package br.com.mello.springscreenmatch;
 
-import br.com.mello.springscreenmatch.model.DadosEpisodio;
-import br.com.mello.springscreenmatch.model.DadosSerie;
 import br.com.mello.springscreenmatch.model.DadosTemporada;
-import br.com.mello.springscreenmatch.service.ConsumoApi;
-import br.com.mello.springscreenmatch.service.ConverteDados;
+import br.com.mello.springscreenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,41 +18,8 @@ public class SpringscreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		//Preparando consumidor da API e conversor:
-		ConsumoApi consulta = new ConsumoApi();
-		ConverteDados conversorDeTipo = new ConverteDados();
-
-		//Pesquisando título:
-		String tituloPesquisado = "Stranger Things".replace(" ", "+");
-		int episodio = 1;
-		int temporada = 1;
-
-		//Montando URI:
-		String chaveApi = "1a6bcfd8";
-		String enderecoURI = "https://www.omdbapi.com/?apikey=" + chaveApi + "&t=" + tituloPesquisado;
-		String enderecoURIEpisodio = enderecoURI + "&season=" + temporada + "&episode=" + episodio;
-		System.out.println(enderecoURIEpisodio);
-		var jsonResultado = consulta.realizarConsulta(enderecoURI);
-		var jsonResultadoDois = consulta.realizarConsulta(enderecoURIEpisodio);
-		
-
-		//Atribuindo dados do Json ao record:
-		DadosSerie dadosSerie = conversorDeTipo.jsonParaObjeto(jsonResultado, DadosSerie.class);
-		System.out.println("Resultado da série: " + dadosSerie);
-		DadosEpisodio dadosEpisodio = conversorDeTipo.jsonParaObjeto(jsonResultadoDois, DadosEpisodio.class);
-		System.out.println("Resultado do episódio" + dadosEpisodio);
-
-		//Listando temporadas:
-		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i < dadosSerie.totalTemporadas(); i++) {
-			String jsonResultadoTemporada = consulta.realizarConsulta(enderecoURI + "&season=" + i);
-			DadosTemporada dadosTemporada = conversorDeTipo.jsonParaObjeto(jsonResultadoTemporada, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		//Ainda não aprendi a sintaxe desse sout:
-		temporadas.forEach(System.out::println);
-
-
+		Principal principal = new Principal();
+		principal.exibirMenu();
 	}
 
 }
---
+++ src/main/java/br/com/mello/springscreenmatch/principal/Principal.java
@@ -0,0 +1,43 @@
+package br.com.mello.springscreenmatch.principal;
+
+import br.com.mello.springscreenmatch.model.DadosSerie;
+import br.com.mello.springscreenmatch.model.DadosTemporada;
+import br.com.mello.springscreenmatch.service.ConsumoApi;
+import br.com.mello.springscreenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private final String ENDERECO = "https://www.omdbapi.com/?apikey=";
+    private final String API_KEY = "1a6bcfd8";
+    private final ConsumoApi consultaAPI = new ConsumoApi();
+    private final Scanner entrada = new Scanner(System.in);
+    private final ConverteDados conversor = new ConverteDados();
+
+    public void exibirMenu(){
+        //Pedindo titulo para pesquisa:
+        System.out.print("Digite nome de um filme ou série: ");
+        var tituloPesquisado = entrada.nextLine();
+        String uri = ENDERECO + API_KEY + "&t=" + tituloPesquisado.replace(" ", "+");
+
+        //Pesquisando com a API:
+        var jsonResultado = consultaAPI.realizarConsulta(uri);
+
+        //Atribuindo dados do Json ao record:
+        DadosSerie dadosSerie = conversor.jsonParaObjeto(jsonResultado, DadosSerie.class);
+        System.out.println("Resultado da série: " + dadosSerie);
+
+        //Listando temporadas:
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i < dadosSerie.totalTemporadas(); i++) {
+            String jsonResultadoTemporada = consultaAPI.realizarConsulta(uri + "&season=" + i);
+            DadosTemporada dadosTemporada = conversor.jsonParaObjeto(jsonResultadoTemporada, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        //Ainda não aprendi a sintaxe desse sout:
+        temporadas.forEach(System.out::println);
+    }
+
+}
