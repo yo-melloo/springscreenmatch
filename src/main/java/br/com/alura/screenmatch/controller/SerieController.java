8a374078771d196496004aec1c1ca84e2e4f71e4
Gustavo Mello <oliver7mello@gmail.com>
2025-01-17 11:58:13 -0300

    Seprando Service de Controller

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.controller;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,16 @@ import java.util.stream.Collectors;
 @RestController
 public class SerieController {
     @Autowired
-    private SerieRepository repositorio;
+    SerieService servico;
+
+    @GetMapping("/")
+    public String retornarAoInicio(){
+        return "Bem-vindo";
+    }
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
-        return repositorio.findAll().stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
     }
+
 }
